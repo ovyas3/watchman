f843be100b3ae350a64cbf803a2b5425da9edce4
Pramod Ukkali <pramod.ukkali@instavans.com>
2023-03-21 09:51:12 +0530

    added return to home button

--- app/completed/page.tsx
+++ app/completed/page.tsx
@@ -1,15 +1,31 @@
 'use client'
+import Link from 'next/link';
 import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring';
 
 function completed() {
     const [completed, setCompleted] = useState(true);
-    const completedSpring = useSpring({
-        opacity: completed ? 1 : 0,
-        transform: completed ? 'scale(1)' : 'scale(0.8)',
-      });
+    const handleComplete = () => {
+
+    };
+
+
+
+
   return (
+    <div className='flex items-center justify-center flex-col w-screen h-screen'>
     <div className='flex items-center justify-center text-[24px] text-blue-600 w-full h-screen'>Completed</div>
+    <Link
+                    href={{
+                        pathname: '/',
+                    }}
+                    className="w-full px-[20px] py-[56px] "
+                >
+                    <div className="button flex items-center justify-center bg-[#2962FF] border rounded-[6px] w-full h-[48px]">
+                        <button className='text-white'>Return to home</button>
+                    </div>
+                    </Link>
+            </div>
   )
 }
 
